273512646ab1f228bf808933836109dd77f62cbb
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-09-13 21:24:11 +0900

    Initial Commit

---
+++ MyCoinPoket/MyCoinPoketApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCoinPoketApp.swift
+//  MyCoinPoket
+//
+//  Created by 이윤지 on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MyCoinPoketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
